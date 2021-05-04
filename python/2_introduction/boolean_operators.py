670a9eac300510ba79bac7a2301e4d75e3f4c543
Vibhor Verma <vibhorverma2015@gmail.com>
2021-05-04 19:59:53 +0530

    adding boolean operators code of python

---
+++ python/2_introduction/boolean_operators.py
@@ -0,0 +1,45 @@
+"""
+Boolean operator (are something which you can put between 2 boolean values)
+
+these are operators which give you either true or false 
+
+it is denoted as (==)
+
+1.) Equality operator / comparison operator
+
+>>> a =1
+>>> b =1
+
+>>> a==b  (true)
+============================
+>>> a=1
+>>> b=2
+>>> c=3
+
+then C==b+a (true)
+
+=================================
+
+2.) GATES
+
+AND gate : and 
+OR gate : or
+NOT gate : not
+
+weather = "sunny"
+temp = "20"
+
+if weather is sunny and temp is 20
+
+AND : if at its both side the conditions are ture then only the answer will be true
+x and y: the answer will be true if both the values i.e. x and y is true
+
+======================================
+
+
+if and else statement
+
+
+"""
+
+print("hey ! please entre an intger")
