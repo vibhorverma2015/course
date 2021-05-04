d564f526500409e6e1fef37996ca9f6315cf6cd5
Vibhor Verma <vibhorverma2015@gmail.com>
2021-05-04 16:05:36 +0530

    calculation codes in python

---
+++ python/introduction/calculation.py
@@ -0,0 +1,27 @@
+"""
+To do the calculations of numbers like additon, substaction , division , multiiplication
+there are several codes in the python which
+is engaged to do so
+
+
+let suppose we have to add two numbers :
+
+>>>  print("Heyy! Please entre the first number")
+first number = float(input())
+
+print("Heyy! Please entre the second number")
+second number = float(input())
+
+print("Total", = first number+second number)
+
+"""
+
+
+
+print ("Hey! Please entre the first number")
+First_number = float(input())
+
+print ("Hey! Please entre the second number")
+Second_number = float(input())
+
+print("Total", First_number+Second_number) 
