6fa7867f41955d7a6e9c48a9fa32c087b6393b5f
Vibhor Verma <vibhorverma2015@gmail.com>
2021-05-04 13:47:59 +0530

    adding 1st lecture of python introduction

---
+++ python/introduction/intro.py
@@ -0,0 +1,28 @@
+"""
+This is the first lecture on python we understand about the basic keywords of python coding like
+types of variable
+finding value
+and many more
+
+we specially learn about the three data bodies
+which are 
+strn
+int
+float
+
+
+
+"""
+
+print("Hello World")
+
+a = 5
+print("value of a", a), print(type(a)) 
+
+b = 15
+print("value of b", b), print(type(b))
+
+c = a-b
+print("value of c", c), print(type(c))
+
+
