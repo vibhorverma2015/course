50bd9a4ff8f6c61f6ada444a34d0b95fc299b0e0
Vibhor Verma <vibhorverma2015@gmail.com>
2021-05-04 14:44:32 +0530

    adding input_output codes

---
+++ python/introduction/input_output.py
@@ -0,0 +1,22 @@
+"""
+In this file we will write the code whih will demonstrate how to take input and output from 
+the user
+
+the command to take input from user is:
+
+>>> user_input = input()
+
+here user_input is a variable which will capture user's input
+and input() is a function which captures user input
+ 
+ and input will always take input as string data
+
+"""
+
+print("Heyy!! please entre some text")
+
+text = input() #the programm will wait here for the user to give some input from them
+
+print(" Heyy!! you entere text", text)
+
+print("The data type of text variable is", type(text)) 
