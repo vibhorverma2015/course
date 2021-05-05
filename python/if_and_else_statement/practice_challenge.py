7e29295e0185e4309a00998a6cd5db238bcaf2aa
Vibhor Verma <vibhorverma2015@gmail.com>
2021-05-05 15:49:32 +0530

    practice challenge of if and else

---
+++ python/if_and_else_statement/practice_challenge.py
@@ -0,0 +1,19 @@
+"""
+If a number is divisible by 3 print FIZZ
+If a number is divisible by 5 print FUZZ
+If a number is divisible by both print FIZZ_BUZZ
+
+"""
+user_input = float(input())
+
+if (user_input % 5 == 0 and user_input % 3 == 0):
+    print("FIZZ_FUZZ")
+else:
+    if (user_input % 5 == 0):
+        print("FUZZ")
+    if (user_input % 3 == 0):
+        print("FIZZ")
+
+   
+    
+
