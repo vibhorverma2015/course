92e75b9ef74c943fc91bfa965ddac915ee59ad4e
Vibhor Verma <vibhorverma2015@gmail.com>
2021-05-05 15:47:58 +0530

    adding if and else exapmple

---
+++ python/if_and_else_statement/if_else_example.py
@@ -0,0 +1,17 @@
+"""
+"""
+
+first_name = "Vibhor"
+last_name = "verma"
+
+if(first_name == "Vibhor"):
+    if(last_name == "Verma"):
+        print("Vibhor is from coding batch")
+    else:
+        if(last_name == "verma"):
+            print("you made a typing error")
+
+        print("Vibhor is not from coding batch")    
+        
+else:
+    print("this student is unknown")
