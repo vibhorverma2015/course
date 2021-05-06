37271059a342a5c3f1ecdb6c357215fa87206208
Vibhor Verma <vibhorverma2015@gmail.com>
2021-05-06 14:57:38 +0530

    inroducing nesting in loops

---
+++ python/loops/nested_for_loops.py
@@ -0,0 +1,13 @@
+"""
+Nested loops are the loops in which the scenario occurs where 
+the ranges of number can be shown with accordanc to it's 
+nested range.
+
+"""
+
+
+for i in range(3):
+    print("j loop start for i", i)
+    for j in range(3):
+        print(i, j)
+    print("j loopp end for i", i)
