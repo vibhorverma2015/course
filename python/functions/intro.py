5f56e0e6fbff7dde2a5376869a9c3d74623f69d3
Vibhor Verma <vibhorverma2015@gmail.com>
2021-05-08 19:35:14 +0530

    introducing functions in python

---
+++ python/functions/intro.py
@@ -0,0 +1,29 @@
+"""
+Functions are some constructs available in programming 
+language by which we can reduce duplicacy in code
+and make our code easy to understand.
+
+def <name_of_a_function>():
+
+once a function is created you can call
+its code by
+<name_of_function>()
+
+"""
+
+
+# def greet():
+#   print("hello everyone")
+#  print("nice to meet you")
+
+def greet():
+    print("hello", user_name)
+    print("nice to meet you")
+
+
+print("my name is vibhor verma")
+
+print("please entre your user_name")
+user_name = input()
+
+greet()
